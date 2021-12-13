c2ac3034e87c8af38d06d5ab0f4955ab60e1e5e3
SriMethan <77402282+SriMethan@users.noreply.github.com>
2021-12-13 06:54:59 +0530

    Update chess.ts

--- client/chess.ts
+++ client/chess.ts
@@ -290,15 +290,11 @@ export const VARIANTS: { [name: string]: Variant } = {
       pieceRoles: ["k", "q", "r", "b", "n", "p"],
       enPassant: true,
       alternateStart: {
-            '': '',
-            'PawnsPushed': "rnbqkbnr/8/8/pppppppp/PPPPPPPP/8/8/RNBQKBNR w KQkq - 0 1",
-            'PawnsPassed': "rnbqkbnr/8/8/PPPPPPPP/pppppppp/8/8/RNBQKBNR w - - 0 1",
-            'UpsideDown': "RNBKQBNR/PPPPPPPP/8/8/8/8/pppppppp/rnbkqbnr w - - 0 1",
-            'Theban': "1p6/2p3kn/3p2pp/4pppp/5ppp/8/PPPPPPPP/PPPPPPKN w - - 0 1",
+          '': '',
+          'PawnsPushed': "rnbqkbnr/8/8/pppppppp/PPPPPPPP/8/8/RNBQKBNR w KQkq - 0 1",
         },
       chess960: true, icon: "♔", icon960: "♔",      
-
-    }),  
+   }),  
 
     makruk: new Variant({
         name: "makruk", tooltip: () => _("Thai Chess. A game closely resembling the original Chaturanga. Similar to Chess but with a different queen and bishop."),
